56dd28fd82a644887dbc22db29038bae6c2bbc99
Ayush Jain <ayushjain17.n@gmail.com>
2021-06-25 21:10:24 +0530

    configured cloud db

--- dbconfig/configData.js
+++ dbconfig/configData.js
@@ -1,6 +1,13 @@
 module.exports = {
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: '',
-    DB: 'ayush',
+    // Local
+    // HOST: 'localhost',
+    // USER: 'root',
+    // PASSWORD: '',
+    // DB: 'ayush',
+
+    // Cloud
+    HOST: 'brj9csqliktgn44ihh0n-mysql.services.clever-cloud.com',
+    USER: 'uoofl5osf5wvlzdp',
+    PASSWORD: 'nFiJwxTk8Rb3OT9WszBs',
+    DB: 'brj9csqliktgn44ihh0n',
 }
