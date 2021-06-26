913858a38a8a25b9b20019d3851f005bca9fc3c9
Ayush Jain <ayushjain17.n@gmail.com>
2021-06-26 19:55:04 +0530

    connection changed to create pool

--- dbconfig/configData.js
+++ dbconfig/configData.js
@@ -1,13 +1,13 @@
 module.exports = {
     // Local
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: '',
-    DB: 'ayush',
+    // HOST: 'localhost',
+    // USER: 'root',
+    // PASSWORD: '',
+    // DB: 'ayush',
 
     // Cloud
-    // HOST: 'brj9csqliktgn44ihh0n-mysql.services.clever-cloud.com',
-    // USER: 'uoofl5osf5wvlzdp',
-    // PASSWORD: 'nFiJwxTk8Rb3OT9WszBs',
-    // DB: 'brj9csqliktgn44ihh0n',
+    HOST: 'brj9csqliktgn44ihh0n-mysql.services.clever-cloud.com',
+    USER: 'uoofl5osf5wvlzdp',
+    PASSWORD: 'nFiJwxTk8Rb3OT9WszBs',
+    DB: 'brj9csqliktgn44ihh0n',
 }
